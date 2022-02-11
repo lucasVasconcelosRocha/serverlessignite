24128b0d5f383a661ad1a0e4455644a6c517de4e
Lucas Vasconcelos <lucas.vasconcelos915@gmail.com>
2022-02-10 23:24:14 -0300

    Criaçao do template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,11 +1,34 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
 import { document } from "../utils/dynamodbClient";
+import { compile } from "handlebars";
+import dayjs from "dayjs";
+
+import { join } from "path";
+
+import { readFileSync } from "fs";
+import chromium from "chrome-aws-lambda";
 
 
 interface ICreateCertificate {
     id: string,
     name: string,
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    medal: string;
+    date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+    const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+    const html = readFileSync(filePath, "utf-8");
+
+    return compile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -29,6 +52,37 @@ export const handler: APIGatewayProxyHandler = async (event) => {
         }
     }).promise();
 
+    const metalPath = join(process.cwd(), "src", "templates", "selo.png");
+    const medal = readFileSync(metalPath, "base64");
+
+    const data: ITemplate = {
+        name,
+        id,
+        grade,
+        date: dayjs().format("DD/MM/YYYY"),
+        medal
+    }
+
+    const content = await compileTemplate(data);
+
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath
+    });
+
+    const page = await browser.newPage();
+
+    await page.setContent(content);
+    const pdf = await page.pdf({
+        format: "a4",
+        landscape: true,
+        printBackground: true,
+        preferCSSPageSize: true,
+        path: process.env.IS_OFFLINE ? "./certificate.pdf" : null
+    })
+
+    await browser.close();
 
     return {
         statusCode: 201,
